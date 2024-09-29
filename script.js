3c148b8d90636b526e7d38041f2188efb9c375b5
Otajk <otavio.souza53@etec.sp.gov.br>
2024-09-29 15:17:53 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function mouseon(){
+    let botao = document.getElementsByClassName("botao");
+    let x = document.getElementsByTagName("svg");
+    var ang = 30;
+    document.botao.style.transform = "rotate("+ ang +"deg)";
+    
+}
+
